7c47203eb19e98de29f7014d6721a3354320a2e4
Vivek Kancharla <65432144+Vivek41916@users.noreply.github.com>
2022-02-09 15:25:01 +0530

    added the naive codes

---
+++ Helper.js
@@ -0,0 +1,8 @@
+function handleRegister() {
+  var reg = (document.getElementById("Register").style.display = "block");
+  var login = (document.getElementById("Loginpage").style.display = "none");
+}
+function handleLogin() {
+  var reg = (document.getElementById("Register").style.display = "none");
+  var login = (document.getElementById("Loginpage").style.display = "block");
+}
